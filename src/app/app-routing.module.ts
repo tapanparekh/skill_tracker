9093c2ef2d5667f0fc7f3f9fb73c5291c72ebe52
WKFS-FRC\Ashok.Yadav <ashok.yadav@wolterskluwer.com>
2023-05-10 17:27:32 +0530

    create emplyees-details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'search-skill', loadChildren: () => import('./search-skill/search-skill.module').then(m => m.SearchSkillModule) },
-{ path: 'user-registration', loadChildren: () => import('./components/user-registration/user-registration-routing.module').then(m => m.UserRegistrationRoutingModule) }];
+const routes: Routes = [
+  {
+    path: 'search-skill',
+    loadChildren: () =>
+      import('./search-skill/search-skill.module').then(
+        (m) => m.SearchSkillModule
+      ),
+  },
+  {
+    path: 'user-registration',
+    loadChildren: () =>
+      import(
+        './components/user-registration/user-registration-routing.module'
+      ).then((m) => m.UserRegistrationRoutingModule),
+  },
+  {
+    path: 'employee-details/:id',
+    loadChildren: () =>
+      import('./employee-details/employee-details.module').then(
+        (m) => m.EmployeeDetailsModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SearchSkillComponent } from './search-skill/search-skill.component';
-// import { SearchComponent } from './search/search.component';
 import { UserRegistrationComponent } from './components/user-registration/user-registration.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserRegistrationComponent,
-    HeaderComponent,
-    // SearchComponent,
-  ],
+  declarations: [AppComponent, UserRegistrationComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgSelectModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
