22a8d0a2e954a9d9ba4062996d835af2e8d84acb
WKFS-FRC\Ashok.Yadav <ashok.yadav@wolterskluwer.com>
2023-05-10 20:03:16 +0530

    add route login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private route: Router) {
+    this.route.navigate(['./login']);
+  }
   title = 'SkillTracker';
 }
